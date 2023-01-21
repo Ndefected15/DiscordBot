ed241acd348f37ddb3e743a0c9f328ec627da575
Nicholas Defex <ndefected15@gmail.com>
2023-01-21 09:59:33 -0500

    CronJob functioning, added bot to test  discord

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
 const { Client } = require('discord.js');
-const cron = require('cron').CronJob;
+const CronJob = require('cron').CronJob;
 
 const client = new Client({
 	intents: [412317132864],
 });
 
-client.on('ready', () => {
+client.once('ready', () => {
 	console.log('BeFrWithMe is online!');
 });
 
 client.login(
-	'MTA2NjM0MjAwMjEyMTI0ODc3OA.GPHBmk.b5fknov_Wjd5h8ugEebHl3Tllu9m3vszRsLAU0'
+	'MTA2NjM0MjAwMjEyMTI0ODc3OA.GJ0-ch.zykcCvHy49bBdVQbKGxwBEeN2cFHjwn7eyMXL0'
 );
 
 function test() {
 	console.log('Action executed.');
+	client.channels.cache.get('befrwithme').send('Hello here!');
 }
 
-let job1 = new cron.CronJob('01 05 01,13 * * *', test); // fires every day, at 01:05:01 and 13:05:01
-let job2 = new cron.CronJob('00 00 08-16 * * 1-5', test); // fires from Monday to Friday, every hour from 8 am to 16
+let job1 = new CronJob('* * * * *', test); // fires every day, at 01:05:01 and 13:05:01
+let job2 = new CronJob('00 00 08-16 * * 1-5', test); // fires from Monday to Friday, every hour from 8 am to 16
 
-job2.start();
+job1.start();
