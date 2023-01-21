67fdd20074a9f1a16d63ed0a84ca87c952626de0
Nicholas Defex <ndefected15@gmail.com>
2023-01-21 13:17:33 -0500

    troubleshooting heroku

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ client.once('ready', async () => {
 
 	const channel = await client.channels.fetch('1066370266780934144');
 
-	let msg = new CronJob('15 13 * * *', send);
+	let msg = new CronJob('20 13 * * *', send);
 
 	function send() {
 		console.log('Working');
