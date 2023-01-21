8feedec0a06a83e627536ccbd920f6590ef7a3b8
Nicholas Defex <ndefected15@gmail.com>
2023-01-21 11:39:51 -0500

    FUNCTIONING

--- main.js
+++ main.js
@@ -5,20 +5,16 @@ const client = new Client({
 	intents: [412317132864],
 });
 
-client.once('ready', () => {
+client.once('ready', async () => {
 	console.log('BeFrWithMe is online!');
+
+	const channel = await client.channels.fetch('1066370266780934144');
+
+	let msg = CronJob('0 12 1/1 * *', () => {
+		channel.send('@here be fr with me rn');
+
+		msg();
+	});
 });
 
-client.login(
-	'MTA2NjM0MjAwMjEyMTI0ODc3OA.GJ0-ch.zykcCvHy49bBdVQbKGxwBEeN2cFHjwn7eyMXL0'
-);
-
-function test() {
-	console.log('Action executed.');
-	client.channels.cache.get('befrwithme').send('Hello here!');
-}
-
-let job1 = new CronJob('* * * * *', test); // fires every day, at 01:05:01 and 13:05:01
-let job2 = new CronJob('00 00 08-16 * * 1-5', test); // fires from Monday to Friday, every hour from 8 am to 16
-
-job1.start();
+client.login('TOKEN');
