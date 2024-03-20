e37ff19edf5111bc5db464b679947574d6d2e440
Nick Okeefe <nickokeefe@Nicks-MacBook-Pro.local>
2024-03-20 19:27:46 -0400

    Added random emoji and theRealest functionality

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const { Client } = require('discord.js');
+const { Client, REST, Routes, SlashCommandBuilder } = require('discord.js');
 const CronJob = require('cron').CronJob;
-
+const botID = '1066342002121248778';
+const serverID = '1066350114219769918';
 const client = new Client({
 	intents: [412317132864],
 });
@@ -18,12 +19,54 @@ client.once('ready', async () => {
 		return Math.floor(Math.random() * (max - min) + min);
 	}
 
-	let msg = new CronJob(
+	const msg = new CronJob(
 		`${minute(59, 0)} ${hour(20, 14)} * * *`,
-		function () {
-			// console.log(hour(20, 14));
-			// console.log(minute(59, 0));
-			channel.send('@here Be fr with me rn <:Big_Iron:795054994457624577>');
+		async function () {
+			const messageArray = [
+				'<:Big_Iron:795054994457624577>',
+				'<:HmmDanger:636268056024449034>',
+				'<a:PartyBear:699413102596325446>',
+				'<:howdy:1190871335203770388>',
+				'<:yos:973470777783500830>',
+				'<:ChoccyMilk:936711481460940800>',
+				'<a:club_penguin:701572269029195806> ',
+				'<:Capybara:954093564067995720>',
+				'<:canIhas:936711440511926372>',
+				'<:gritty:902949839111868458> ',
+			];
+
+			const random = Math.floor(Math.random() * messageArray.length);
+
+			if (!channel) return console.error('Invalid channel ID.');
+
+			channel.send(`@here Be fr with me rn ${messageArray[random]}`);
+
+			const theRealest = new CronJob('0 * * * * *', async function () {
+				const userIDs = [];
+
+				const messages = await channel.messages.fetch({ limit: 100 }); // Fetch last 100 messages
+				const now = Date.now();
+				const oneHourAgo = now - 60 * 60 * 1000;
+
+				messages.forEach((message) => {
+					if (
+						message.attachments.size > 0 &&
+						message.createdTimestamp >= oneHourAgo
+					) {
+						userIDs.push(message.author.id);
+					}
+				});
+
+				if (userIDs.length === 0) return console.log('No user IDs found.');
+
+				const randomizer = userIDs[Math.floor(Math.random() * userIDs.length)];
+
+				channel.send(`<@${randomizer}> is the realest today`);
+			});
+
+			setTimeout(() => {
+				theRealest.start();
+			}, 3600000); // Start theRealest job after one hour
 		},
 		null,
 		true,
@@ -31,6 +74,39 @@ client.once('ready', async () => {
 	);
 
 	msg.start();
+});
+
+const rest = new REST().setToken(botToken);
+const slashRegister = async () => {
+	try {
+		await rest.put(Routes.applicationGuildCommands(botID, serverID), {
+			body: [
+				new SlashCommandBuilder()
+					.setName('')
+					.setDescription('just a simple ping command')
+					.addStringOption((option) => {
+						return option
+							.setName('message')
+							.setDescription('the message to send')
+							.setRequired(false);
+					}),
+			],
+		});
+	} catch (error) {}
+};
+
+slashRegister();
+
+client.on('interactionCreate', async (interaction) => {
+	if (!interaction.isCommand()) return;
+
+	const userId = interaction.user.id;
+	console.log(`User ID: ${userId}`);
+
+	if (interaction.commandName === 'ping') {
+		const msg = interaction.options.getString('message');
+		await interaction.reply(`You sent me: ${msg}`);
+	}
 });
 
 client.login(process.env.DJS_TOKEN);
---
+++ slash-commands.js
@@ -0,0 +1,19 @@
+// const { REST, Routes } = require('discord.js');
+// const botID = '1066342002121248778';
+// const serverID = '1066350114219769918';
+
+// const rest = new REST().setToken(botToken);
+// const slashRegister = async () => {
+// 	try {
+// 		await rest.put(Routes.applicationGuildCommands(botID, serverID), {
+// 			body: [
+// 				{
+// 					name: 'ping',
+// 					description: 'just a simple ping command',
+// 				},
+// 			],
+// 		});
+// 	} catch (error) {}
+// };
+
+// export { slashRegister };
